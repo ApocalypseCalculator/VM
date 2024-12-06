295fdb98b5a7879a26c65e0197ab52005a276e71
Patrick Lin <patrick@tensoropera.com>
2024-12-05 21:11:20 -0500

    add w command

--- action/move.cc
+++ action/move.cc
@@ -2,6 +2,7 @@
 #include "../model/vmstate.h"
 #include "../model/cursor.h"
 #include <ncurses.h>
+#include "../model/cursor.h"
 
 MoveCursor::MoveCursor() {
     mode = Mode::NORMAL;
@@ -16,17 +17,51 @@ void MoveCursor::doAction(const std::vector<int> &input, VMState *vmstate) {
         vmstate->getFileState()->moveCursor(-1, 0);
     } else if(input.at(0) == KEY_RIGHT || input.at(0) == 'l') {
         vmstate->getFileState()->moveCursor(1, 0);
+    } else if(input.at(0) == 'w') {
+        FileState *file = vmstate->getFileState();
+        Cursor search = file->getCursor();
+        bool foundws = false;
+        while(search.lineidx < file->getLineCount()) {
+            std::string line = file->getLine(search.lineidx);
+            char nextchar;
+            // finding next character within line
+            if(search.charidx+1 < line.size()) {
+                search.charidx++;
+                nextchar = line.at(search.charidx);
+            } else {
+                // newline
+                // last character of last line
+                if(search.lineidx == file->getLineCount()-1) {
+                    if(search.charidx < line.size()) search.charidx++; // go to last character
+                    break;
+                }
+                search.charidx = 0;
+                search.lineidx++;
+                foundws = true; // found a newline
+                if(file->getLine(search.lineidx).size() > 0) {
+                    nextchar = file->getLine(search.lineidx).at(0);
+                }
+            }
+            if(nextchar == ' ' || nextchar == '\t') {
+                foundws = true;
+            } else if(foundws) {
+                // we reached the first character after whitespace
+                break;
+            }
+        }
+        file->setCursor(search);
     }
     vmstate->getController()->flushBuffer();
 }
 
+static const int validInputs[] = {KEY_UP, KEY_DOWN, KEY_LEFT, KEY_RIGHT, 'h', 'j', 'k', 'l', 'w'};
+
 bool MoveCursor::matchAction(const std::vector<int> &input) {
     if(input.size() == 1) {
-        if(input.at(0) == KEY_UP || input.at(0) == KEY_DOWN || input.at(0) == KEY_LEFT || input.at(0) == KEY_RIGHT) {
-            return true;
-        }
-        if(input.at(0) == 'h' || input.at(0) == 'j' || input.at(0) == 'k' || input.at(0) == 'l') {
-            return true;
+        for(auto &i : validInputs) {
+            if(input.at(0) == i) {
+                return true;
+            }
         }
     }
     return false;
--- controller/curseskb.cc
+++ controller/curseskb.cc
@@ -13,6 +13,13 @@ CursesKeyboard::CursesKeyboard() {
 Action* CursesKeyboard::getAction() {
     int ch = getch();
     buffer.push_back(ch);
+    if(ch == 27) { // double escape key press
+        if(buffer.size() >= 2 && buffer.at(buffer.size()-2) == 27) {
+            buffer.clear();
+            mode = Mode::NORMAL;
+            return nullptr;
+        }
+    }
     return parser.parseAction(buffer, mode);
 }
 
